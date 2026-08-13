2561a2940284e101d3c767d3e776371052300f37
PinQusestion <mananbansal238@gmail.com>
2026-08-13 21:14:21 +0530

    Add tasks database migration

---
+++ server/migrations/20260813153038_create_tasks_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex) {
+  return knex.schema.createTable("tasks", function (table) {
+    table.increments("id").primary();
+    table.string("title", 255).notNullable();
+    table.text("description");
+    table.boolean("completed").defaultTo(false);
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("tasks");
+};
